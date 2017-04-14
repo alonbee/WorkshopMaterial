5f010589e4d054bdbc7035de382ca89ac07b918c
Allenwu <chimian.wu@gmail.com>
2017-04-14 16:05:09 -0400

    finish

--- js/init.js
+++ js/init.js
@@ -4,4 +4,10 @@
 // this initializes all modals so they can be triggered
 $(document).ready(function() {
   //initializing code goes here!
+  $(".dropdown-button").dropdown({ hover: false });
 });
+
+$('.modal').modal();
+
+// parallax code from: http://materializecss.com/parallax.html
+$('.parallax').parallax();
